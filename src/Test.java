32c6dd2d798cc182acc7ba1f1c498355232a4b49
AleksandrZhurba <a.zhurbinho@gmail.com>
2015-05-08 13:47:07 +0300

    Lesson1 complet

--- src/Test.java
+++ src/Test.java
@@ -1,6 +1,13 @@
+import com.sun.org.apache.xpath.internal.SourceTree;
 
 public class Test {
     public static void main (String args[]) {
-        System.out.println("ololol");
+
+        GetValue myNum = new GetValue();
+
+        System.out.println(myNum.min(7,5,3));
+        System.out.println(myNum.max(7, 5, 3));
+        System.out.println(myNum.average(7, 5, 3));
+
     }
 }
