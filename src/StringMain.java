6e1cada89d3039464ccb1e3efe0b32abefc1445f
AleksandrZhurba <a.zhurbinho@gmail.com>
2015-05-13 17:38:16 +0300

    lesson_string

---
+++ src/StringMain.java
@@ -0,0 +1,12 @@
+public class StringMain {
+
+    public static void main (String[] args) {
+
+        StringUtils str = new StringUtils();
+        String omg = "Vertical Text In Word. Making text vertical in word isn’t easy if you don’t know where to look. There is no simple way of getting Word (word) to display vertically aligned text, but there is a sneaky way";
+
+        str.findWord(omg);
+
+        
+    }
+}
---
+++ src/StringUtils.java
@@ -0,0 +1,21 @@
+public class StringUtils {
+
+    public void div (String numOne, String numTwo) {
+
+
+    }
+
+    public void findWord (String text){
+
+        String s = text.replaceAll("W", "w");
+        String s1 = s.replaceAll("word", "nerd");
+
+        System.out.println(s1);
+
+    }
+
+    public void findNumber (String text) {
+
+
+    }
+}
